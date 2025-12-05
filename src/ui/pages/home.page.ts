e301af39b653718b37689207ea625250719ed090
UlyanaGr <uliana.grechikha@gmail.com>
2025-12-05 23:02:32 +0300

    lection_27

--- src/ui/pages/home.page.ts
+++ src/ui/pages/home.page.ts
@@ -1,23 +1,22 @@
 import { Locator } from "@playwright/test";
 import { SalesPortalPage } from "./salesPortal.page";
 
-type HomeModuleButton = "Products" | "Customers" | "Orders";
+export type HomeModuleButton = "Products" | "Customers" | "Orders";
+
 export class HomePage extends SalesPortalPage {
   readonly welcomeText = this.page.locator(".welcome-text");
   readonly productsButton = this.page.locator("#products-from-home");
   readonly customersButton = this.page.locator("#customers-from-home");
   readonly ordersButton = this.page.locator("#orders-from-home");
   readonly uniqueElement = this.welcomeText;
-  readonly ordersThisYear = this.page.locator("#total-orders-container p");
-  readonly newCustomers = this.page.locator("#total-customers-container p");
-  readonly cancelledOrders = this.page.locator("#canceled-orders-container p");
 
   async clickOnViewModule(module: HomeModuleButton) {
     const moduleButtons: Record<HomeModuleButton, Locator> = {
       Products: this.productsButton,
       Customers: this.customersButton,
-      Orders: this.ordersButton
+      Orders: this.ordersButton,
     };
+
     await moduleButtons[module].click();
   }
 }
